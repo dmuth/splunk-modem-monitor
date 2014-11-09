f1bd4cd4ce8bec63eb8c382f8934bdcb7951e4b4
Douglas Muth <dmuth@dmuth.org>
2014-11-08 19:07:03 -0500

    Fixed issue with PHP not working right under Splunk

---
+++ bin/arris.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# This wrapper script seems silly, but the reason why we have it is because 
+# otherwise we'll get errors like this from PHP:
+#
+# /opt/splunk/lib/libssl.so.1.0.0: version `OPENSSL_1.0.0' not found
+#
+
+
+#
+# Errors are fatal
+#
+set -e
+
+unset LD_LIBRARY_PATH
+pushd `dirname $0` > /dev/null
+
+./arris.php
+
+
